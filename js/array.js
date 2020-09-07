aff97bd891a8693e45a9d6af5fba9da047baa000
jsf5077 <jsf5077@gmail.com>
2020-09-07 16:34:18 -0400

    full functionality on linking the array to the html gallery and dynamically creating the images on the static webpage using jquery

--- js/array.js
+++ js/array.js
@@ -1,20 +1,20 @@
 const array = [
   {
-    image: "/img/pdk-g/gallery-1.jpg",
+    image: "img/pdk-g/gallery-1.jpg",
     thumbnail: "/img/pdk-g/gallery-1.jpg",
     info1: "PDK",
     info2: "Contracting",
     imageAlt: "Image 1",
   },
   {
-    image: "/img/pdk-g/gallery-2.jpg",
+    image: "img/pdk-g/gallery-2.jpg",
     thumbnail: "/img/pdk-g/gallery-2.jpg",
     info1: "PDK",
     info2: "Contracting",
     imageAlt: "Image 2",
   },
   {
-    image: "/img/pdk-g/gallery-3.jpg",
+    image: "img/pdk-g/gallery-3.jpg",
     thumbnail: "/img/pdk-g/gallery-3.jpg",
     info1: "PDK",
     info2: "Contracting",
--- js/gallery.js
+++ js/gallery.js
@@ -1,73 +1,52 @@
-// Here we are building the URL we need to query the database
-
 import { gallery } from "./array.js";
 
 var array = gallery();
 
-console.log(array);
+// console.log(array[i]);
 
-// for (i = 0, i < result.gallery.length; i++; ) {
-//   console.log(array[i]);
-// }
+for (var i = 0; i < array.length; i++) {
+  console.log(array[i]);
 
-// var response = result.gallery;
-// // var artistURL = $("<a>").attr("href", response.url).append(artistName);
-// var info = $("<h2>").text(
-//   $("<i>").text(response.info1, $("<span>").text(response.info2))
+  $("#gallery-plug").append(
+    "<a href='" +
+      array[i].image +
+      "'>" +
+      "<figure class='effect-honey tm-gallery-item'>" +
+      "<img src='img/gallery-tn-06.jpg' alt='" +
+      array[i].imageAlt +
+      "' class='img-fluid' />" +
+      "<figcaption><h2><i>" +
+      array[i].info1 +
+      "<br><span>" +
+      array[i].info2 +
+      "</span></i></h2></figcaption></figure>"
+  );
+}
+
+// $("#gallery-plug").append(
+//   "<a href='img/gallery-img-01.jpg'>" +
+//     "<figure class='effect-honey tm-gallery-item'>" +
+//     "<img src='" +
+//     array[i].image +
+//     "' alt='" +
+//     array[i].imageAlt +
+//     "' class='img-fluid' />" +
+//     "<figcaption><h2><i>" +
+//     array[i].info1 +
+//     "<span>" +
+//     array[i].info2 +
+//     "</span></i></h2></figcaption></figure>"
 // );
 
-// var figcaption = $("<figcaption>").text(info);
-
-// var img = $("<img>").attr({
-//   src: response.thumbnail,
-//   alt: response.imageAlt,
-//   class: "img-fluid",
-// });
-
-// var figure = $("<figure>")
-//   .attr("class", "effect-honey tm-gallery-item")
-//   .text(img, figcaption);
-
-// var images = $("<a>").attr("href", response.image).text(figure);
-
-// // console.log(result);
-// console.log(response);
-
-// $("#gallery-plug").append(images);
-// });
-// });
-
-// $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-// $(".wind").text("Wind Speed: " + response.wind.speed);
-// $(".humidity").text("Humidity: " + response.main.humidity);
-// $(".temp").text("Temperature (F) " + response.main.temp);
-
-// // Log the data in the console as well
-// console.log("Wind Speed: " + response.wind.speed);
-// console.log("Humidity: " + response.main.humidity);
-// console.log("Temperature (F): " + response.main.temp);
-
-// /////////////////////////////
-
-// // Constructing HTML containing the artist information
-// var artistName = $("<h1>").text(response.name);
-// var artistURL = $("<a>").attr("href", response.url).append(artistName);
-// var artistImage = $("<img>").attr("src", response.thumb_url);
-// var trackerCount = $("<h2>").text(
-//   response.tracker_count + " fans tracking this artist"
-// );
-// var upcomingEvents = $("<h2>").text(
-//   response.upcoming_event_count + " upcoming events"
-// );
-// var goToArtist = $("<a>").attr("href", response.url).text("See Tour Dates");
-
-// // Empty the contents of the artist-div, append the new artist content
-// $("#artist-div").empty();
-// $("#artist-div").append(
-//   artistURL,
-//   artistImage,
-//   trackerCount,
-//   upcomingEvents,
-//   goToArtist
-// );
-// });
+/* <a href="img/gallery-img-01.jpg">
+  <figure class="effect-honey tm-gallery-item">
+    <img src="img/gallery-tn-01.jpg" alt="Image 1" class="img-fluid" />
+    <figcaption>
+      <h2>
+        <i>
+          Physical Health <span>Exercise!</span>{" "}
+        </i>
+      </h2>
+    </figcaption>
+  </figure>
+</a>; */
