63ecd07cef243a91b644b0e3cb05b7d8328bcc10
jsf5077 <jsf5077@gmail.com>
2020-09-07 15:41:54 -0400

    started to create an array which will be used to store the info needed for the picture gallery. created communications between this array.js and gallery.js which will build out the necessary html components needed via jquery

---
+++ js/array.js
@@ -0,0 +1,27 @@
+const array = [
+  {
+    image: "/img/pdk-g/gallery-1.jpg",
+    thumbnail: "/img/pdk-g/gallery-1.jpg",
+    info1: "PDK",
+    info2: "Contracting",
+    imageAlt: "Image 1",
+  },
+  {
+    image: "/img/pdk-g/gallery-2.jpg",
+    thumbnail: "/img/pdk-g/gallery-2.jpg",
+    info1: "PDK",
+    info2: "Contracting",
+    imageAlt: "Image 2",
+  },
+  {
+    image: "/img/pdk-g/gallery-3.jpg",
+    thumbnail: "/img/pdk-g/gallery-3.jpg",
+    info1: "PDK",
+    info2: "Contracting",
+    imageAlt: "Image 3",
+  },
+];
+
+export function gallery() {
+  return array;
+}
